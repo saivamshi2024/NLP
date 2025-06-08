24d31f4ee8fa7e281cb4aa51ff8359e29b33c0d1
AVULAMANDA SAI VAMSHI <138317286+saivamshi2024@users.noreply.github.com>
2025-06-08 22:08:51 +0530

    Update lang_translation.py

--- MULTIPLE LANGUAGE TRANSLATION/lang_translation.py
+++ MULTIPLE LANGUAGE TRANSLATION/lang_translation.py
@@ -6,7 +6,7 @@ from gtts import gTTS
 import base64
 
 # read language dataset
-df = pd.read_csv(r'C:\Users\A3MAX SOFTWARE TECH\A VS CODE\11. CAPSTONE PROJECT_DEPLOYMENT\MULTIPLE LANGUAGE TRANSLATION\language.csv')
+df = pd.read_csv(r'C:\Users\Admin\A VS CODE\NLP\MULTIPLE LANGUAGE TRANSLATION\language.csv')
 df.dropna(inplace=True)
 lang = df['name'].to_list()
 langlist=tuple(lang)
